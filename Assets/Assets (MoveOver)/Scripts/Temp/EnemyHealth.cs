69a0de532a344c5edace54d8944543bae6393165
vaporiiise <142332680+vaporiiise@users.noreply.github.com>
2025-06-10 12:52:04 +0800

    u0.3
    -added alleyway

---
+++ Assets/Assets (MoveOver)/Scripts/Temp/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int enemyMaxHealth = 100;
+    public int enemyCurrentHealth;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
